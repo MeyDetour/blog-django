67abd75196c20dd834092a4c2a4aeee6718b35de
mey <meydetour@gmail.com>
2024-11-06 15:55:00 +0100

    edit settings

--- blog/settings.py
+++ blog/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': "djangoblog",
         "USER":  "mey",
-        "PASSWORD":  "deleteProduct",
+        "PASSWORD":  "rVOG23VhmpGLQF4c",
         "HOST":  "localhost",
         "PORT":  5432,
     }
